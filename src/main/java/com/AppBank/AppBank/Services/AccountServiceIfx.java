d1f4f276a715479bd025e744aac978bc8100d806
Kenya Gonzalez <kenya.gonzalez@solera.com>
2023-07-05 10:50:17 +0200

    Finished basic CRUD for account

--- src/main/java/com/AppBank/AppBank/Services/AccountServiceIfx.java
+++ src/main/java/com/AppBank/AppBank/Services/AccountServiceIfx.java
@@ -5,14 +5,18 @@ import com.AppBank.AppBank.entities.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface AccountServiceIfx {
 
-    Account saveAccount(Account account);
+    Account saveAccount(Account account) throws Exception;
+
+    List<Account> findAll();
 
     Account findAccountById(Long id) throws Exception;
 
-    Account updateAccount(Long id);
+    Account updateAccount(Long id, Account account) throws Exception;
 
     void deleteAccountById(Long id);
 
--- src/main/java/com/AppBank/AppBank/Services/AccountServiceImp.java
+++ src/main/java/com/AppBank/AppBank/Services/AccountServiceImp.java
@@ -4,6 +4,7 @@ import com.AppBank.AppBank.Repositories.AccountRepository;
 import com.AppBank.AppBank.entities.Account;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,9 +16,17 @@ public class AccountServiceImp implements AccountServiceIfx{
     }
 
     @Override
-    public Account saveAccount(Account account) {
+    public Account saveAccount(Account account) throws Exception {
+        if(account == null) {
+            throw new Exception("account cannot be empty");
+        }
         accountRepo.save(account);
         return account;
+    }
+
+    @Override
+    public List<Account> findAll(){
+        return accountRepo.findAll();
     }
 
     @Override
@@ -30,12 +39,25 @@ public class AccountServiceImp implements AccountServiceIfx{
     }
 
     @Override
-    public Account updateAccount(Long id) {
-        return null;
+    public Account updateAccount(Long id, Account account) throws Exception {
+        Optional<Account> updatedAccOp = accountRepo.findById(id);
+
+        if(updatedAccOp.isEmpty()){
+            throw new Exception("Account not found");
+        }
+        Account updatedAccount = updatedAccOp.get();
+
+        updatedAccount.setName(account.getName());
+        updatedAccount.setBalance(account.getBalance());
+        updatedAccount.setTransactions(account.getTransactions());
+        //TODO: make sure only admins can change owners, not the clients
+        updatedAccount.setOwner(account.getOwner());
+
+        return updatedAccount;
     }
 
     @Override
     public void deleteAccountById(Long id) {
-
+        accountRepo.deleteById(id);
     }
 }
