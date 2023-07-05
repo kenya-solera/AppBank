3fdd45fa0801ef2f0e0ea04f14446ff718ede2fa
Cristina0606 <cristina.sanz@solera.com>
2023-07-05 11:10:30 +0200

    I have created the user class, the UserRepository interface, the UserController class and the UserService class. I am in the process of filling them all

--- src/main/java/User/UserService.java
+++ src/main/java/User/UserService.java
@@ -1,6 +1,25 @@
 package User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-im
 
+@Service
 public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
+
+
 }
+
+
+
+
+
+
+
